b4a5e15bc115c09ec82535c05e7d2f951266a62e
sanmarr26 <104732441+SantiCode41@users.noreply.github.com>
2025-08-30 00:28:29 -0500

    Add getters and setters to User.java

--- backend/src/main/java/com/financial/finApp/entity/User.java
+++ backend/src/main/java/com/financial/finApp/entity/User.java
@@ -16,5 +16,39 @@ public class User {
 
     @Column(name="password")
     private String password;
+
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
