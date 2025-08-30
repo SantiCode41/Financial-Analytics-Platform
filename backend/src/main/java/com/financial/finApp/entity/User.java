719963df57f82fbfb1c6ae5248c98fd4fdac4499
sanmarr26 <104732441+SantiCode41@users.noreply.github.com>
2025-08-30 00:17:26 -0500

    Create User entity

--- backend/src/main/java/com/financial/finApp/entity/User.java
+++ backend/src/main/java/com/financial/finApp/entity/User.java
@@ -1,7 +1,20 @@
 package com.financial.finApp.entity;
 
-//@Entity
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
 }
 
